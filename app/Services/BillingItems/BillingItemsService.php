01abe9e31577f7ecd13f6abe5b8d77a18744be42
Lubomir <lubomirgeorgie394@gmail.com>
2025-05-05 14:11:47 +0300

    1. Creating billing item service which will work with the itemParser and itemProvier and it will return the parsed items which will be ready to be sent to stripe

---
+++ app/Services/BillingItems/BillingItemsService.php
@@ -0,0 +1,37 @@
+<?php 
+
+namespace App\Services\BillingItems;
+
+use App\Services\BillingItems\Contracts\ItemParser;
+use App\Services\BillingItems\Contracts\ItemProvider;
+
+class BillingItemsService
+{
+    private ItemParser $itemParser;
+    private ItemProvider $itemProvider;
+    private array $billingItems;
+    private string $identifier;
+
+    public function __construct(ItemParser $itemParser, ItemProvider $itemProvider, $user_id)
+    {
+        $this->itemParser = $itemParser;
+        $this->itemProvider = $itemProvider;
+        $this->getBillingItems($user_id);
+    }
+
+    protected function getBillingItems($user_id): void
+    {
+        $this->billingItems = $this->itemProvider->get($user_id);
+        $this->identifier = $this->itemProvider->getIdentifier();
+    }
+
+    public function getIdentifier(): string
+    {
+        return $this->identifier;
+    }
+
+    public function getParsedBillingItems(): array
+    {
+        return $this->itemParser->parse($this->billingItems);
+    }
+}
